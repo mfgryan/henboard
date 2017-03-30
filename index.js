7311a858cd11c5cebcd9f91d0b06213823117f36
mfgryan <ryanbhenao@gmail.com>
2017-03-29 22:22:12 -0400

    got all core functions working. will Dockerize next

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-const http = require('http'),
-    fs = require('fs');
+const http = require('http'), fs = require('fs'), qs = require('querystring');
 
 var hostname = "localhost";
 var port = 8000;
 
-console.log("server running on http://"+ hostname + ":" + port);
-http.createServer(function(request, response) {  
-    
-    //set route settings for url
-    var fileName = "";
-    var contentType = "";
-    
-    var routes = {
+var routes = {
         "/api": {
             fileName: "./api.json",
             contentType: "application/json"
@@ -28,21 +20,41 @@ http.createServer(function(request, response) {
             fileName: "./index.html",
             contentType: "text/html"
         }
-    };
+};
+
+console.log("server running on http://"+ hostname + ":" + port);
+
+http.createServer(function(request, response) {  
     var route = request.url in routes ? routes[request.url] : routes["/"];
-
-    //if api post with data 
-    //write to file
     
-    //else read the file and respond 
-    fs.readFile(route.fileName, function (err, file) {
-        if (err) {
-            throw err; 
-        }       
-        console.log(request.url + " 200 - " + route.fileName);
-        response.writeHead(200, {"Content-Type": route.contentType});  
-        response.write(file);  
-        response.end();  
+    var body = '';
+    request.on('data', function (data) {
+        body += data;
+        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+        if (body.length > 1e6) { 
+            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+            request.connection.destroy();
+        }
     });
-
+    
+    request.on('end', function () {
+        //var data = qs.parse(body); in case you need to access object properties
+        if(request.url == "/api" && request.method == "POST"){
+            fs.writeFile(route.fileName,body,function(err){
+                if (err) throw err;
+                console.log(request.url + " 200 - " + route.fileName + " " + request.method)
+                response.writeHead(200);
+                response.end(); 
+            }); 
+        }else{
+            fs.readFile(route.fileName, function (err, file) {
+                if (err) throw err; 
+                console.log(request.url + " 200 - " + route.fileName + " " + request.method);
+                response.writeHead(200, {"Content-Type": route.contentType});  
+                response.write(file);  
+                response.end();  
+            });
+        }
+    });
+    
 }).listen(port,hostname);
--- main.js
+++ main.js
@@ -1,75 +1,155 @@
-var main = function (){
-    // ajax api request to the json data
-    // hard coded for now
-    var data = [
-    {
-        week: "03/11/17", 
-            todo: [], 
-            development: [],
-            completed: []
-    },
-    {
-        week: "03/17/17",
-        todo: [], 
-        development: [],
-        completed: []
-    },
-    {
-        week: "03/25/17",
-        todo: [], 
-        development: [],
-        completed: []
-    }];
-
-    var sprint = data[data.length-1]; 
-
-    var populateBoard = function(){
-        $('#todo .list').empty().append(sprint.todo.join(''));
-        $('#development .list').empty().append(sprint.development.join(''));
-        $('#completed .list').empty().append(sprint.completed.join('')); 
-    };
+var date = (function(){
+        
+    var _MILLISECONDS_IN_DAY = 1000 * 60 * 60 * 24;
     
-    var addData = function(column){
-        var record = $("#" + column + " .insert input[type=text]").val();    
-        sprint[column].push("<p>"+record+
-                "<span class=\"remove\" onclick=\"main.removeData($(this).parent(),$(this).parents().eq(2).attr('id'))\"> &nbsp; x </span></p>");
-        populateBoard();
-    };
-
-    var removeData = function(node, column){ 
-        sprint[column].splice($(node).index(), 1);
-        populateBoard();
-    };
-
-    var setSprint = function(node){
-        sprint = data[data.length-1 - $('#weeks').prop('selectedIndex')];
-        populateBoard();
-    };
-
-    var populateOptions = function (){
-        var weeks = $('#weeks');    
-        for(var i = data.length-1; i >= 0; i--){
-            weeks.append("<option value=\""+ data[i].week +"\">week "+ data[i].week +"</option>");
+    return {
+        getMonday: function (d){
+            d = new Date(d);
+            var day = d.getDay(),
+            diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+            return new Date(d.setDate(diff)); 
+        },
+        getDateFormat: function(date){
+            var dd = date.getDate(); 
+            var mm = date.getMonth() + 1;
+            var yy = date.getFullYear().toString().substr(-2);
+            dd = dd < 10 ? '0'+dd : dd;
+            mm = mm < 10 ? '0'+mm : mm;
+            return mm+'/'+dd+'/'+yy;
+        },
+        dayDiff: function (a , b){
+            var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+            var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+            return Math.floor((utc2 - utc1) / (_MILLISECONDS_IN_DAY)); 
         }
-        $('#weeks option').eq(0).prop('selected', true);
+    }
+}());
+
+var io = (function (){
+    var _writeQueue = [];
+    return {
+        readData: function(callBack){
+            $.ajax({
+                url: "/api",
+                dataType: "json",
+                success: callBack,
+                error: function(){
+                    console.log("read error!");
+                    callBack([]);
+                }
+            });
+        },
+        writeData: function(){
+            _writeQueue.push(function(){
+                console.log("read request!");
+                $.ajax({
+                    url: "/api",
+                    type: "POST",
+                    dataType: "json",
+                    data: JSON.stringify(board.getData()),
+                    success: function(){
+                        _writeQueue.shift();
+                        if(_writeQueue.length > 0 ) _writeQueue[0]();
+                    },
+                    error: function(){
+                        console.log("write error!");
+                        _writeQueue.shift();
+                    }
+                });
+            })
+            if(_writeQueue.length == 1) _writeQueue[0]();
+        }
     };
+}());
 
-    populateOptions();
-
-    //generate week function
-    //see if >= 14 days from most recent week
-    //if true data.push new week object
-
-    //drag and drop function
-    //save data to json on changes
+var board = (function (){
+    
+    var _data = [];
+    
+    var _sprint = {};
 
     return {
-        data: data,
-        sprint: sprint,
-        populateBoard: populateBoard,
-        addData: addData,
-        removeData: removeData,
-        setSprint, setSprint
-    }
-}();
-main.populateBoard();
+        setData: function(data){
+            _data = !data ? [] : data; 
+            this.checkWeek();
+            this.setSprint(0);
+        },
+        // set which sprint is currently displayed on the board
+        setSprint: function(index){
+            _sprint = _data.length > 0 ? _data[_data.length-1 - index] : {};
+            this.populateBoard();
+        },
+        getData: function(){
+            return _data; 
+        },
+        // populates the board with data from the current sprint 
+        populateBoard: function(){
+            $('#todo .list').empty().append(_sprint.todo.join(''));
+            $('#development .list').empty().append(_sprint.development.join(''));
+            $('#completed .list').empty().append(_sprint.completed.join('')); 
+        },
+        // add a new task to the given column and current sprint
+        addTask: function(column, val){
+            debugger;
+            _sprint[column].push("<p draggable=\"true\" ondragstart=\"board.dragTask(event,$(this).index(), $(this).parents().eq(1).attr('id'))\">"+
+                    "<span class=\"task\">"+val+"</span>"+
+                    "<span class=\"remove\" onclick=\"board.removeTask($(this).parents().eq(2).attr('id'),$(this).parent().index())\"> &nbsp; x </span></p>");
+            this.populateBoard();
+            io.writeData();
+        },
+        // remove a task selected by index, column, and current sprint
+        removeTask: function(column, index){ 
+            _sprint[column].splice(index, 1);
+            this.populateBoard();
+            io.writeData();
+        },
+        // if data is empty or it's been more than 14 days, push a new sprint object
+        checkWeek: function(){
+            var curDate = new Date(); 
+            var latestDate = _data.length > 0 ? new Date(_data[_data.length-1].week) : curDate;
+            
+            if(_data.length <= 0 || date.dayDiff(latestDate, curDate) >= 14){
+                _data.push({
+                    week: date.getDateFormat(date.getMonday(curDate)),
+                    todo: [],
+                    development: [],
+                    completed: []
+                });
+                this.setSprint(0);
+                this.populateBoard();
+                io.writeData();
+            }
+        },
+        // populate the options tag with a list of sprint weeks
+        populateOptions: function (){
+            var weeks = $('#weeks');    
+            for(var i = _data.length-1; i >= 0; i--){
+                weeks.append("<option value=\""+ _data[i].week +"\">week "+ _data[i].week +"</option>");
+            }
+            $('#weeks option').eq(0).prop('selected', true);
+        },
+        // when a task is dragged save it's column and index
+        dragTask: function(e, index, column){
+            e.dataTransfer.setData("index", index);     
+            e.dataTransfer.setData("columnFrom", column);
+        },
+        // when a task is dropped delete the old task and add the new task
+        dropTask: function(e, columnTo){
+            e.preventDefault(); 
+            var columnFrom = e.dataTransfer.getData("columnFrom");
+            var index = e.dataTransfer.getData("index"); 
+            var val = $('#'+columnFrom+' .list p').eq(index).children('.task').html();
+            this.removeTask(columnFrom, index);    
+            this.addTask(columnTo, val);
+        },
+        allowDrop: function(e){
+            e.preventDefault();    
+        }
+    };
+}());
+
+io.readData(function(data){
+    board.setData(data); 
+    board.populateOptions();
+    board.populateBoard();
+});
