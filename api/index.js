f163116eb70f973e5cc81d78ddd7822ad26aba99
mfgryan <ryanbhenao@gmail.com>
2017-06-11 12:18:12 -0400

    basic routes defined for express.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,28 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    res.send('Henapi!');
+});
+
+app.get('/projects', function(req, res) {
+    res.send('projects');
+});
+
+app.get('/sprints', function(req, res) {
+    res.send('sprints!');
+});
+
+app.get('/info', function(req, res) {
+    res.send('info!');
+});
+
+
+app.get('/items', function(req, res) {
+    res.send('items');
+});
+
+app.get('/lanes', function(req, res) {
+    res.send('lanes!');
 });
 
 app.listen(8080, function () {
