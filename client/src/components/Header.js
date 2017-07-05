c8f3f484b7bd2380400b1f02f20c2b7a86e1b43c
mfgryan <ryanbhenao@gmail.com>
2017-07-05 14:32:59 -0400

    dropdown items on home page menu bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 // bootstrap dep
-import { Row, Col, Alert } from "react-bootstrap";
+import { Row, Col, Alert, DropdownButton, MenuItem } from "react-bootstrap";
 
 //css dep
 import "../css/Header.css";
@@ -12,17 +12,26 @@ const Header = ( { project, messages, removeMessages } ) => {
     return (
         <Row>
             <Col md={2}>
-                <h1>≡</h1>
-            </Col>
-            <Col md={8}>
-                <h1>{ project }</h1>
-            </Col>
-            <Col md={2}>
                 { messages.length > 0 &&
                 <Alert className="errorMessage" bsStyle="danger">
                     <strong>Error. </strong> { messages[0] }
                 </Alert>
                 }
+            </Col>
+            <Col md={8}>
+                <h1>{ project }</h1>
+            </Col>
+            <Col md={2}>
+                <DropdownButton className="dropDownMenu" noCaret title="≡" >
+                    <MenuItem key="home" eventKey="home">home</MenuItem>
+                    <MenuItem key="backlog" eventKey="backlog">backlog</MenuItem>
+                    <MenuItem key="planning" eventKey="planning">planning</MenuItem>
+                    <MenuItem divider />
+                    <MenuItem key="undo" eventKey="undo">undo <small>(← key)</small></MenuItem>
+                    <MenuItem key="redo" eventKey="redo">redo <small>(→ key)</small></MenuItem>
+                    <MenuItem divider />
+                    <MenuItem key="settings" eventKey="settings">settings</MenuItem>
+                </DropdownButton>
             </Col>
         </Row>
     )
