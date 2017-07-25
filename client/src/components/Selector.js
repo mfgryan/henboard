553aedc960b31a8afcdd1fdf3f84e1c2679c0b81
mfgryan <ryanbhenao@gmail.com>
2017-07-25 08:17:26 -0400

    load all capability to selector

--- client/src/components/Selector.js
+++ client/src/components/Selector.js
@@ -1,24 +1,37 @@
 // react dep
-import React from "react";
+import React, { Component } from "react";
 import { Col, Row, DropdownButton, MenuItem } from "react-bootstrap";
 
 // style dep
 import "../css/Selector.css";
 
-const Selector = ( { title, items, itemKey, addItemTitle, itemClicked} ) => {
-    return (
-        <Row className="Selector">
-            <Col md={12}>
-                <DropdownButton noCaret onSelect={(event) => itemClicked(event)} id="Selector" title={title}>
-                    {items.map((item, index) =>
-                        <MenuItem key={item[itemKey]} eventKey={item[itemKey]}>{index+1}&nbsp;&nbsp;<small>{item[itemKey]}</small></MenuItem>
-                    )}
-                    <MenuItem divider />
-                    <MenuItem eventKey={"add"}>{addItemTitle}</MenuItem>
-                </DropdownButton>
-            </Col>
-        </Row>
-    )
+class Selector extends Component { 
+    constructor(props){
+        super(props);
+        this.state = {showAll: false};
+    }
+    render() {
+        let range = this.state.showAll ? this.props.items.length : 3 ;
+        return (
+            <Row className="Selector">
+                <Col md={12}>
+                    <DropdownButton noCaret onSelect={(event) => this.props.itemClicked(event)} id="Selector" title={this.props.title}>
+                        {this.props.items.reverse().slice(0,range).map((item, index) =>
+                            <MenuItem key={item[this.props.itemKey]} eventKey={item[this.props.itemKey]}>
+                                {this.props.items.length-index}&nbsp;&nbsp;<small>{item[this.props.itemKey]}</small>
+                            </MenuItem>
+                        )}
+                        {!this.state.showAll && 
+                            <li>
+                                <a href="#" onClick={() => this.setState({showAll: true})}>load all</a>
+                            </li>}
+                        <MenuItem divider />
+                        <MenuItem eventKey={"add"}>{this.props.addItemTitle}</MenuItem>
+                    </DropdownButton>
+                </Col>
+            </Row>
+            )
+    }
 }
 
 export default Selector; 
