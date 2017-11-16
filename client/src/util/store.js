3c7809576ff7946d465b71c67f8347075e24a186
mfgryan <ryanbhenao@gmail.com>
2017-11-15 20:16:40 -0800

    commented logger

--- client/src/util/store.js
+++ client/src/util/store.js
@@ -3,4 +3,5 @@ import { createStore, applyMiddleware } from "redux";
 import { logger, validate, fn } from "../util/middleware.js"; // eslint-disable-line no-unused-vars
 import rootReducer from "../reducers/reducers.js";
 
-export default createStore(rootReducer, applyMiddleware(validate, logger, fn));
+// export default createStore(rootReducer, applyMiddleware(validate, logger, fn));
+export default createStore(rootReducer, applyMiddleware(validate, fn));
